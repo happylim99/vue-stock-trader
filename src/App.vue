bbf220cbebcc8498eb14c8d883e8901bc7785774
SeanLim <happylim99@gmail.com>
2019-10-17 14:22:12 +0800

    Header navbar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
 	<div>
+		<app-header />
 		<router-view />
 	</div>
 </template>
 
 <script>
+import Header from './components/layouts/Header'
 export default {
-
+	components: {
+		appHeader: Header
+	}
 }
 </script>
 
--- src/components/layouts/Header.vue
+++ src/components/layouts/Header.vue
@@ -1,6 +1,36 @@
 <template>
 	<div>
-		<h1>The Header component</h1>
+		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+			<router-link to="/" class="navbar-brand">Stock Trader</router-link>
+
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav mr-auto">
+					<!-- <li class="nav-item"> -->
+						<router-link to="/portfolio" class="nav-item nav-link" active-class="active"><a>Portfolio</a></router-link>
+					<!-- </li> -->
+					<li class="nav-item">
+						<router-link to="/stocks" class="nav-link" active-class="active"><a>Stocks</a></router-link>
+					</li>
+				</ul>
+				<ul class="navbar-nav">
+					<router-link to="#" class="nav-item nav-link" active-class="active"><a>End day</a></router-link>
+					<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Save & Load</a>
+					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+						<a class="dropdown-item" href="#">Save Data</a>
+						<a class="dropdown-item" href="#">Load Data</a>
+						<div class="dropdown-divider"></div>
+						<a class="dropdown-item" href="#">Something else here</a>
+					</div>
+					</li>
+				</ul>
+
+				<form class="form-inline my-2 my-lg-0">
+					<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+					<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+				</form>
+			</div>
+		</nav>
 	</div>
 </template>
 
@@ -11,5 +41,8 @@ export default {
 </script>
 
 <style scoped>
-
+.navbar {
+	padding-left: 50px;
+	padding-right: 50px;
+}
 </style>
